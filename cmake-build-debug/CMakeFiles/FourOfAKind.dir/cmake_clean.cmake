d774f53e4c6e229b7097465071beb83a243b994f
Rustam <rusbag90@gmail.com>
2020-10-16 11:14:56 +0300

    Adding New Project

---
+++ cmake-build-debug/CMakeFiles/FourOfAKind.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FourOfAKind.dir/main.cpp.obj"
+  "FourOfAKind.exe"
+  "FourOfAKind.exe.manifest"
+  "FourOfAKind.pdb"
+  "libFourOfAKind.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FourOfAKind.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
